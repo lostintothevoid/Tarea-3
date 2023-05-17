3e8660d6fa2a7009d35740f7a4733193bb4e4cef
lostintothevoid <127423332+lostintothevoid@users.noreply.github.com>
2023-05-17 15:45:30 +0000

    se realizo un avance en la funcion importar
    la funcion importar se encarga de agregar datos de un archivo a la aplicacion todavia esta en fase de pruebas debido a que en esta misma se realiza el ingreso se espera de que de funcionar bien se importe el archivo esto fasilitara el testeo de las demas funciones sin mas que comentar saludos

--- .ccls-cache/@home@runner@Tarea-3/main.c
+++ .ccls-cache/@home@runner@Tarea-3/main.c
@@ -6,7 +6,7 @@
 #include "heap.h"
 #include "list.h"
 #include "Map.h"
-/*La struct tarea se encarga de guardar el nombre de la tarea y su prioridad.
+//La struct tarea se encarga de guardar el nombre de la tarea y su prioridad.
 typedef struct{
   char taskname[100];
   int prioridad; 
@@ -25,7 +25,7 @@ typedef struct{
   tarea* task;//La tarea en si.
   List* prevTasks; //Lista de tareas previas, por prioridad.
 }tipoTareas;//La estructura tipoTareas guarda todo lo relacionado a la tarea.
-*/
+
 int is_equal_string(void * key1, void * key2) {
     if(strcmp((char*)key1, (char*)key2)==0) return 1;
     return 0;
@@ -39,11 +39,12 @@ void confirmarCaracteres(char *cadena){
     getchar();
   } 
 }
-typedef struct tarea {
-  char taskname[100];
-  int prioridad;
-  struct tarea* precedente;
-} tarea;
+
+// typedef struct tarea {
+//   char taskname[100];
+//   int prioridad;
+//   struct tarea* precedente;
+// } tarea;
 
 typedef struct Nodo {
   tarea* dato;
@@ -55,42 +56,48 @@ typedef struct List {
   int size;
 } List;
 
-typedef struct {
-  int accion;
-  tarea* pilaTask;
-  tarea* pilaTask2;
-  bool interruptor;
-} pilaAcciones;
+// typedef struct {
+//   int accion;
+//   tarea* pilaTask;
+//   tarea* pilaTask2;
+//   bool interruptor;
+// } pilaAcciones;
 
-typedef struct {
-  bool taskflag;
-  tarea* task;
-  List* prevTasks;
-  pilaAcciones* actions; // Se agregó el miembro actions de tipo pilaAcciones
-} tipoTareas;
+// typedef struct {
+//   bool taskflag;
+//   tarea* task;
+//   List* prevTasks;
+//   pilaAcciones* actions; // Se agregó el miembro actions de tipo pilaAcciones
+// } tipoTareas;
 
 //fucion 1 
 void ingresarTarea(Map* tareas){
-  tipoTareas* trashTask = malloc(sizeof(tipoTareas));
-  trashTask->task = malloc(sizeof(tarea));
-  printf("Ingrese la tarea que desea guardar:\n");
-  scanf("%[^\n]s", trashTask->task->taskname);
-  getchar();
-  confirmarCaracteres(trashTask->task->taskname);
-  printf("Ingrese la prioridad de su tarea:\n");
-  scanf("%i", &trashTask->task->prioridad);
-  trashTask->taskflag=true;
+  if(!firstMap(tareas)){
+    tipoTareas* trashTask = malloc(sizeof(tipoTareas));
+    trashTask->task = malloc(sizeof(tarea));
+    printf("Ingrese la tarea que desea guardar:\n");
+    scanf("%[^\n]s", trashTask->task->taskname);
+    getchar();
+    confirmarCaracteres(trashTask->task->taskname);
+    printf("Ingrese la prioridad de su tarea:\n");
+    scanf("%i", &trashTask->task->prioridad);
+    trashTask->taskflag=true;
   
-  trashTask->prevTasks=createList();
+    trashTask->prevTasks=createList();
 
-  trashTask->actions=createStack();
-  // pilaAcciones* pila;
-  // pila->accion=0;
-  // pila->iterruptor=true;
-  // pila->pilaTask=trashTask->task;
-  // push(trashTask->actions, pila);
+    trashTask->actions=createStack();
+    // pilaAcciones* pila;
+    // pila->accion=0;
+    // pila->iterruptor=true;
+    // pila->pilaTask=trashTask->task;
+    // push(trashTask->actions, pila);
   
-  insertMap(tareas, trashTask->task->taskname, trashTask);
+    insertMap(tareas, trashTask->task->taskname, trashTask);
+  }
+  else{
+    return;
+  }
+
   
 }
 
@@ -122,7 +129,7 @@ void cambioDeOrden(Map* tareas){
   }
 }
 
-//Función 3
+/*Función 3
 // Función para mostrar las tareas pendientes ordenadas por prioridad y precedencia
 void mostrarTareasPendientes(tipoTareas* tareas) {
   printf("Tareas por hacer, ordenadas por prioridad y precedencia:\n");
@@ -169,11 +176,117 @@ void mostrarTareasPendientes(tipoTareas* tareas) {
 
   // Liberar memoria
   free(tareasPendientes);
+}*/
+
+const char *get_csv_field(char *tmp, int k, char separator1, char separator2) {
+    static char *line = NULL;
+    static int line_size = 0;
+    int open_mark = 0;
+    char *ret = (char *) malloc(100 * sizeof(char));
+    int ini_i = 0, i = 0;
+    int j = 0;
+    char separator = separator1;
+    if (line == NULL) {
+        line_size = strlen(tmp);
+        line = (char *) malloc((line_size + 1) * sizeof(char));
+        strcpy(line, tmp);
+    } else if (strcmp(line, tmp) != 0) {
+        line_size = strlen(tmp);
+        line = (char *) realloc(line, (line_size + 1) * sizeof(char));
+        strcpy(line, tmp);
+    }
+    while (line[i + 1] != '\0') {
+        if (line[i] == '\"') {
+            open_mark = 1 - open_mark;
+            if (open_mark) ini_i = i + 1;
+            i++;
+            continue;
+        }
+        if (open_mark || (line[i] != separator1 && line[i] != separator2)) {
+            if (k == j) ret[i - ini_i] = line[i];
+            i++;
+            continue;
+        }
+        if (line[i] == separator1 || line[i] == separator2) {
+            if (k == j) {
+                ret[i - ini_i] = 0;
+                return ret;
+            }
+            j++;
+            ini_i = i + 1;
+            if (line[i] == separator1) {
+                separator = separator1;
+            } else {
+                separator = separator2;
+            }
+        }
+        i++;
+    }
+    if (k == j) {
+        ret[i - ini_i] = 0;
+        return ret;
+    }
+    return NULL;
 }
+
+void importarArchivo(Map *tareas, Heap *orden){
+  char archivo[100];
+  printf("Ingrese el nombre del archivo que quiere ingresar:\n");
+  fflush(stdin);
+  scanf("%[^\n]s",archivo);
+  getchar();
+  FILE *fp=fopen(archivo, "r");
+  if(fp==NULL){
+    printf("===============================================================\n");
+    printf("                   Error al importar archivo...\n");
+    printf("     Asegúrese de importar al programa con el mismo nombre\n");
+    printf("===============================================================\n");
+    return;
+  }
+  char linea[801];
+  fgets(linea,800,fp);
+  while(fgets(linea,800,fp)!=NULL){
+    tipoTareas *auxTarea=malloc(sizeof(tipoTareas));
+    int j=0;
+    while(get_csv_field(linea, j, ',', ' ')!=NULL){
+      char *aux =(char *) get_csv_field(linea, j, ',', ' ');
+      if(aux==NULL) break;
+      
+      if(j==0){
+        strcpy(auxTarea->task->taskname,aux);
+        // Se crea su pila de acciones
+        auxTarea->actions = createStack();
+      }
+      if(j==1){
+        //Se guardan los puntos de habilidad
+        auxTarea->task->prioridad=atoi(aux);
+        
+        pilaAcciones*accion=malloc(sizeof(pilaAcciones));
+        accion->accion=0;
+        accion->pilaTask->prioridad=auxTarea->task->prioridad;
+        //Se guarda la acción en la pila de acciones
+        push(auxTarea->actions,accion);
+      }
+      if(j==2){
+        //Se le asigna la cantidad de task al tarea
+        auxTarea->prevTasks=createList();
+        pushBack(auxTarea->prevTasks, aux);
+      }
+      j++;
+      insertMap(tareas, auxTarea->task->taskname, auxTarea);
+    }
+  }
+  printf("===============================================================\n");
+  printf("        La importación de tareaes fue hecha con éxito\n");
+  printf("===============================================================\n");
+  fclose(fp);
+}
+
 void menu(Map *tareas, Heap* ordenPrio){
 
   int opcion = 1;
   while(opcion != 0){
+    printf("\033[0;46m");
     printf("╔════════════════════════════•°🜧°•════════════════════════════╗\n");
     printf("║ Si desea agregar una alguna tarea presione 1                ║\n");
     printf("║ Si desea cambiar el orden de de dos tareas presione 2       ║\n");
@@ -183,7 +296,7 @@ void menu(Map *tareas, Heap* ordenPrio){
     printf("║ Si desea cargar las tareas a un archivo de texto presione 6 ║\n");
     printf("║ Si no desea hacer nada más presione 0                       ║\n");
     printf("╚════════════════════════════•°🜥°•════════════════════════════╝\n\n");
-    
+    printf("\033[0;40m");
     scanf("%d", &opcion);
     getchar();
 
@@ -194,16 +307,16 @@ void menu(Map *tareas, Heap* ordenPrio){
       case 2: cambioDeOrden(tareas);
       break;
           
-      case 3: mostrarTareasPendientes(tareas);
-      break;
+      // case 3: mostrarTareasPendientes(tareas, ordenPrio);
+      // break;
 
-      // case 4: EliminarItem(jugadores, mapaItems);
+      // case 4: EliminarItem(tareaes, mapatask);
       // break;
       
-      // case 5: agregarPh(jugadores);
+      // case 5: agregarPh(tareaes);
       // break;
         
-      // case 6: ItemEspecifico(jugadores,mapaItems);
+      // case 6: ItemEspecifico(tareaes,mapatask);
       // break;
 
       case 0:
